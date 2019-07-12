6de8057b55aef1a28537f9f1758ff4ccfb0dac4f
unknown <fahreziadh@gamil.com>
2019-07-13 01:55:57 +0700

    to String

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post('/webhook', (req, res) => {
         // Gets the message. entry.messaging is an array, but 
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
-        let message = webhook_event.message;
+        let message = webhook_event.message.toString();
         let sender_psid = webhook_event.sender.id;
         console.log(webhook_event);
         
