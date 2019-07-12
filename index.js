022076b5756b91777f1c61fb02a03cb1cc8782e8
unknown <fahreziadh@gamil.com>
2019-07-13 05:52:32 +0700

    change

--- index.js
+++ index.js
@@ -87,7 +87,34 @@ function getProduct(sender_psid,received_message){
         }
        console.log(err);
       }
-      var item_list = fs.readFileSync('layout/item_list.json', 'utf8')
+      var item_list = {
+        "template_type":"generic",
+        "elements":[
+           {
+            "title":"Welcome!",
+            "image_url":"https://petersfancybrownhats.com/company_image.png",
+            "subtitle":"We have the right hat for everyone.",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://petersfancybrownhats.com/view?item=103",
+              "messenger_extensions": false,
+              "webview_height_ratio": "tall",
+              "fallback_url": "https://petersfancybrownhats.com/"
+            },
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://petersfancybrownhats.com",
+                "title":"View Website"
+              },{
+                "type":"postback",
+                "title":"Start Chatting",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+              }              
+            ]      
+          }
+        ]
+}
       console.log(item_list);
       
        response = {
