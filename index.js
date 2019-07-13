4ca2d6c89cf665af473fdd373f0f6cb836e0722d
unknown <fahreziadh@gamil.com>
2019-07-13 07:36:49 +0700

    payload

--- index.js
+++ index.js
@@ -87,10 +87,71 @@ function getProduct(sender_psid,received_message){
         }
        console.log(err);
       }
-      
-
+      const list = {
+        "template_type": "list",
+        "top_element_style": "compact",
+        "elements": [
+            {
+                "title": "Classic T-Shirt Collection",
+                "subtitle": "See all our colors",
+                "image_url": "http://pngimg.com/uploads/tshirt/tshirt_PNG5450.png",
+                "buttons": [
+                    {
+                        "title": "View",
+                        "type": "web_url",
+                        "url": "https://yudiz-bot.herokuapp.com/collection",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://yudiz-bot.herokuapp.com"
+                    }
+                ]
+            },
+            {
+                "title": "Classic White T-Shirt",
+                "subtitle": "See all our colors",
+                "default_action": {
+                    "type": "web_url",
+                    "url": "https://yudiz-bot.herokuapp.com/view?item=100",
+                    "messenger_extensions": false,
+                    "webview_height_ratio": "tall"
+                }
+            },
+            {
+                "title": "Classic Blue T-Shirt",
+                "image_url": "http://pngimg.com/uploads/tshirt/tshirt_PNG5450.png",
+                "subtitle": "100% Cotton, 200% Comfortable",
+                "default_action": {
+                    "type": "web_url",
+                    "url": "https://yudiz-bot.herokuapp.com/view?item=101",
+                    "messenger_extensions": true,
+                    "webview_height_ratio": "tall",
+                    "fallback_url": "https://yudiz-bot.herokuapp.com"
+                },
+                "buttons": [
+                    {
+                        "title": "Shop Now",
+                        "type": "web_url",
+                        "url": "https://yudiz-bot.herokuapp.com/shop?item=101",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://yudiz-bot.herokuapp.com"
+                    }
+                ]
+            }
+        ],
+        "buttons": [
+            {
+                "title": "View More",
+                "type": "postback",
+                "payload": "payload"
+            }
+        ]
+    }
        response = {
-         "text":body.toString().substring(0,20)
+         "attachment":{
+           "type":"template",
+           "payload":list
+         }
       }
     
     console.log("success to request "+received_message);
